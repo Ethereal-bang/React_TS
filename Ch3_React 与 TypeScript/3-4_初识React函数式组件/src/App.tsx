fdbac9e285ea124b0324c56a5e0aca94d61ab05b
Ethereal-bang <70147063+Ethereal-bang@users.noreply.github.com>
2021-06-10 23:02:36 +0800

    update annotation about "key"
    // 添加 key 是为了解决 Warning: Each child in a list should have a unique “key” prop 的 warning

--- Ch3_React 与 TypeScript/3-4_初识React函数式组件/src/App.tsx
+++ Ch3_React 与 TypeScript/3-4_初识React函数式组件/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     return (
         <ul>
             {robots.map((r, index) => (    // 此处的 () 是为了包裹 jsx 标签，可省略
-                <Robot key={index} id={r.id} email={r.email} name={r.name} />
+                <Robot key={index} id={r.id} email={r.email} name={r.name} />   // 添加 key 是为了解决 Warning: Each child in a list should have a unique “key” prop 的 warning
             ))}
         </ul>
     );
