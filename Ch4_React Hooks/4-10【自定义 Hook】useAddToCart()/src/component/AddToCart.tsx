4118404dad68726f41b5c93dfd4f07df9a02cbe8
Ethereal-bang <70147063+Ethereal-bang@users.noreply.github.com>
2021-06-14 16:43:41 +0800

    Add files via upload

---
+++ Ch4_React Hooks/4-10【自定义 Hook】useAddToCart()/src/component/AddToCart.tsx
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+import { appSetStateContext  } from "../AppState";
+import { RobotProps } from "./Robot";
+
+export const withAddToCart = (ChildComponent: React.ComponentType<RobotProps>) => {
+    // return class extends React.Component {} // 返回类式组件
+    return (props) => { // 返回函数式组件
+        const setState = useContext(appSetStateContext);
+        const addToCart = (id, name) => {
+            if (setState) { 
+                setState((state) => {
+                    return {
+                        ...state,       
+                        shoppingCart: {
+                            items: [...state.shoppingCart.items, {id, name}],   
+                        },
+                    };
+                });
+            }
+        }
+        return <ChildComponent {...props} addToCart={addToCart} />  
+    }
+}
+
+export const useAddToCart = () => {
+    const setState = useContext(appSetStateContext);
+    const addToCart = (id, name) => {
+        if (setState) { 
+            setState((state) => {
+                return {
+                    ...state,       
+                    shoppingCart: {
+                        items: [...state.shoppingCart.items, {id, name}],   
+                    },
+                };
+            });
+        }
+    }
+    return addToCart;
+}
