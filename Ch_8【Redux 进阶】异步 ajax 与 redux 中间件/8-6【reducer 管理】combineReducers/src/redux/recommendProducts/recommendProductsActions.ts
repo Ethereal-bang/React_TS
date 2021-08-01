beed8ade00b2102a2e7d0717ce2ff6eac969cccb
Ethereal-bang <70147063+Ethereal-bang@users.noreply.github.com>
2021-08-01 11:05:19 +0800

    modify variable name and add types

--- Ch_8【Redux 进阶】异步 ajax 与 redux 中间件/8-6【reducer 管理】combineReducers/src/redux/recommendProducts/recommendProductsActions.ts
+++ Ch_8【Redux 进阶】异步 ajax 与 redux 中间件/8-6【reducer 管理】combineReducers/src/redux/recommendProducts/recommendProductsActions.ts
@@ -2,42 +2,41 @@ export const FETCH_RECOMMEND_PRODUCTS_START = 'FETCH_RECOMMEND_PRODUCTS_START';
 export const FETCH_RECOMMEND_PRODUCTS_SUCCESS = 'FETCH_RECOMMEND_PRODUCTS_SUCCESS';
 export const FETCH_RECOMMEND_PRODUCTS_FAIL = 'FETCH_RECOMMEND_PRODUCTS_FAIL';
 
-interface FetchRecommendProductStartAction {
-	type: typeof FETCH_RECOMMEND_PRODUCTS_START;
+interface FetchRecommendProductsStartAction {
+    type: typeof FETCH_RECOMMEND_PRODUCTS_START;
 }
 
-interface FetchRecommendProductSuccessAction {
-	type: typeof FETCH_RECOMMEND_PRODUCTS_SUCCESS;
-	payload: any;	// API 调用成功后返回的数据
+interface FetchRecommendProductsSuccessAction {
+    type: typeof FETCH_RECOMMEND_PRODUCTS_SUCCESS;
+    payload: any;	// API 调用成功后返回的数据
 }
 
-interface FetchRecommendProductFailAction {
-	type: typeof FETCH_RECOMMEND_PRODUCTS_FAIL;
-	payload: any;	// API 调用失败后返回的数据
+interface FetchRecommendProductsFailAction {
+    type: typeof FETCH_RECOMMEND_PRODUCTS_FAIL;
+    payload: any;	// API 调用失败后返回的数据
 }
 
 export type RecommendProductsAction =
-	| FetchRecommendProductStartAction
-	| FetchRecommendProductSuccessAction
-	| FetchRecommendProductFailAction;
+    | FetchRecommendProductsStartAction
+    | FetchRecommendProductsSuccessAction
+    | FetchRecommendProductsFailAction;
 
-
-export const fetchRecommendProductsStartActionCreator = () => {
-	return {
-		type: FETCH_RECOMMEND_PRODUCTS_START,
-	};
+export const fetchRecommendProductsStartActionCreator = (): FetchRecommendProductsStartAction => {
+    return {
+        type: FETCH_RECOMMEND_PRODUCTS_START,
+    };
 };
 
-export const fetchRecommendProductsSuccessActionCreator = (data) => {
-	return {
-		type: FETCH_RECOMMEND_PRODUCTS_SUCCESS,
-		payload: data,
-	};
+export const fetchRecommendProductsSuccessActionCreator = (data): FetchRecommendProductsSuccessAction => {
+    return {
+        type: FETCH_RECOMMEND_PRODUCTS_SUCCESS,
+        payload: data,
+    };
 };
 
-export const fetchRecommendProductsFailActionCreator = (error) => {
-	return {
-		type: FETCH_RECOMMEND_PRODUCTS_FAIL,
-		payload: console.error,
-	};
+export const fetchRecommendProductsFailActionCreator = (error): FetchRecommendProductsFailAction => {
+    return {
+        type: FETCH_RECOMMEND_PRODUCTS_FAIL,
+        payload: error,
+    };
 };
