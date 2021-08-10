3ca8ab47a7674a9f68cf39b95f144737529b26d2
Ethereal-bang <70147063+Ethereal-bang@users.noreply.github.com>
2021-08-10 15:22:50 +0800

    Add files via upload

---
+++ Ch_10【顶部导航】用户登录/10-3【注册页面】Antd + TS 表单处理/src/pages/signIn/SignInPage.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { UserLayout } from "../../layouts/userLayout"
+
+
+export const SignInpage : React.FC = () => {
+    return <UserLayout>
+        <h1>登录页面</h1>
+    </UserLayout>
+}
