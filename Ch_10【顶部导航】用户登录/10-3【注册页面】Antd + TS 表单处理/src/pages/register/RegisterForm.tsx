d103d05bc4d17db9c319cf7825bb2288fcbde9b8
Ethereal-bang <70147063+Ethereal-bang@users.noreply.github.com>
2021-08-10 17:42:06 +0800

    add:   `layout`、`tailout`

--- Ch_10【顶部导航】用户登录/10-3【注册页面】Antd + TS 表单处理/src/pages/register/RegisterForm.tsx
+++ Ch_10【顶部导航】用户登录/10-3【注册页面】Antd + TS 表单处理/src/pages/register/RegisterForm.tsx
@@ -1,5 +1,5 @@
-import { Form, Input, Button, Checkbox } from 'antd';
-import styles from './RegisterPage.module.css';
+import { Form, Input, Button, Checkbox } from "antd";
+import styles from "./RegisterForm.module.css";
 
 const layout = {
   labelCol: { span: 8 },
@@ -10,100 +10,67 @@ const tailLayout = {
 };
 
 export const RegisterForm = () => {
-    const onFinish = (values) => {
-        console.log('Success:', values);
-    };
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
 
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
 
-    };
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      className={styles["register-form"]}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
 
-    return (
-        <Form
-            name="basic"
-            labelCol={{
-                span: 8,
-            }}
-            wrapperCol={{
-                span: 16,
-            }}
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            className={styles['register-form']}
-        >
-            <Form.Item
-                label="Username"
-                name="username"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your username!',
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
 
-            <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                ]}
-            >
-                <Input.Password />
-            </Form.Item>
+      <Form.Item
+        label="Confirm Password"
+        name="confirm"
+        hasFeedback
+        rules={[
+          { required: true, message: "Please input your confirm password!" },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue("password") === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject("密码确认不一致！");
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
 
-            <Form.Item
-                label="Confirm Password"
-                name="confirm"
-                hasFeedback
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your confirm password!',
-                    },
-                    (({getFieldValue}) => ({
-                        validator(_, value) {
-                            if (!value || getFieldValue("password") === value) {
-                                return Promise.resolve();
-                            }
-                            return Promise.reject("密码确认不一致！");
-                        }
-                    }))
-                ]}
-            >
-                <Input.Password />
-            </Form.Item>
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
 
-            <Form.Item
-                name="remember"
-                valuePropName="checked"
-                wrapperCol={{
-                    offset: 8,
-                    span: 16,
-                }}
-            >
-                <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item
-                wrapperCol={{
-                    offset: 8,
-                    span: 16,
-                }}
-            >
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
-    );
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
