0c67a0cf204394f3ee12cb7006a17ebe36fcafd3
Ethereal-bang <70147063+Ethereal-bang@users.noreply.github.com>
2021-08-10 21:02:03 +0800

    Add files via upload

---
+++ Ch_10【顶部导航】用户登录/10-4【注册页面】注册业务逻辑处理/src/App.tsx
@@ -0,0 +1,23 @@
+import styles from "./App.module.css";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HomePage, SignInpage, RegisterPage, DetailPage, SearchPage } from './pages';
+
+function App() {
+    return (
+        <div className={styles.App}>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' component={HomePage} />
+                    <Route path='/signIn' component={SignInpage} />
+                    <Route path='/register' component={RegisterPage} />
+                    <Route path='/detail/:touristRouteId' component={DetailPage} />
+                    <Route path="/search/:keywords?" component={SearchPage} />
+                    <Route render={() => <h1>404 not found 页面去火星了！</h1>} />
+                </Switch>
+
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
