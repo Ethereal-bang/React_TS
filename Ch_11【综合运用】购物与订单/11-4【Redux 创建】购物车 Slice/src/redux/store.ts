d7b1bdb03f525fd8056513ae758d16165de4bfaa
Ethereal-bang <70147063+Ethereal-bang@users.noreply.github.com>
2021-09-01 16:55:47 +0800

    Add files via upload

---
+++ Ch_11【综合运用】购物与订单/11-4【Redux 创建】购物车 Slice/src/redux/store.ts
@@ -0,0 +1,42 @@
+import languageReducer from "./language/languageReducer";
+import recommendProductsReducer from "./recommendProducts/recommendProductsReducer";
+import { actionLog } from './middlewares/actionLog';
+import { productDetailSlice } from "./productDetail/slice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { productSearchSlice } from "./productSearch/slice";
+import { UserSlice } from "./user/slice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { shoppingCartSlice } from "./shoopingCart/slice";
+
+const persistConfig = {
+    key: 'root',    // 数据根目录
+    storage,        // 数据保存方式，localStorage
+    whitelist: ['user'] // 白名单
+}
+
+const rootReducer =  combineReducers({
+    language: languageReducer,
+    recommendProducts: recommendProductsReducer,
+    productDetail: productDetailSlice.reducer,
+    productSearch: productSearchSlice.reducer,
+    user: UserSlice.reducer,
+    shoppingCart: shoppingCartSlice.reducer,
+})
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), actionLog],
+    devTools: true, 
+})
+
+const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>
+
+const rootStore = {
+    store, 
+    persistor,
+}
+export default rootStore;
